a9fb785018741066f9d4aa9d672c12dafc54695f
Lokhy87 <a.bolta@gmail.com>
2025-03-18 15:33:46 +0100

    63.5

---
+++ BIBLIOTECA/63.5/class/libro.php
@@ -0,0 +1,42 @@
+<?php
+require_once("publicacion.php");
+
+class Libro extends Publicacion {
+    protected $paginas;
+
+    function __construct($titulo, $autor, $año, $paginas){
+        parent::__construct($titulo, $autor, $año);
+        $this->paginas = $paginas;
+    }
+
+    // Getter
+    public function getPaginas(){
+        return $this->paginas;
+    }
+
+    // Setter
+    public function setPaginas($paginas){
+        $this->paginas = $paginas; 
+    }        
+
+    // Convierte un objeto a un array
+    public function toArray(): array {
+        return [
+            'titulo' => $this->titulo,
+            'autor' => $this->autor,
+            'año' => $this->año,
+            'paginas' => $this->paginas
+        ];
+    }
+
+    // Devuelve un objeto de un array
+    public static function fromArray(array $data): Libro {
+        return new Libro($data['titulo'], $data['autor'], $data['año'], $data['paginas']);
+    }
+
+    public function print() {
+        parent::print();
+        echo "Tipo: $this->paginas<br>";
+    }
+
+}
---
+++ BIBLIOTECA/63.5/class/publicacion.php
@@ -0,0 +1,42 @@
+<?php
+class Publicacion {
+    protected $titulo;
+    protected $autor;
+    protected $año;
+
+    function __construct($titulo, $autor, $año){
+        $this->titulo = $titulo;
+        $this->autor = $autor; 
+        $this->año = $año;
+    }
+
+    // Getters
+    public function getTitulo(){
+        return $this->titulo;
+    }
+
+    public function getAutor(){
+        return $this->autor;
+    }
+
+    public function getAño(){
+        return $this->año;
+    }
+
+    // Setters
+    public function setTitulo($titulo){
+        $this->titulo = $titulo;
+    }
+
+    public function setAutor($autor){
+        $this->autor = $autor;
+    }
+
+    public function setAño($año){
+        $this->año = $año;
+    }
+
+    public function print(){
+        echo "Titulo: $this->titulo; Autor: $this->autor; Año: $this->año; ";
+    }
+}
